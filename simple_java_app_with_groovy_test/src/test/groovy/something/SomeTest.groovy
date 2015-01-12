32b0c9ca399e0d8d2725629de3251c01760f7094
sinan <sinangoo@gmx.de>
2015-01-12 09:52:55 +0100

    added a groovy junit test

---
+++ simple_java_app_with_groovy_test/src/test/groovy/something/SomeTest.groovy
@@ -0,0 +1,15 @@
+package something;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SomeTest {
+
+    @Test
+    public void doSomeTest() {
+        System.out.println("SomeTest:doSomeTest");
+        assertTrue(true);
+    }
+
+}
+
