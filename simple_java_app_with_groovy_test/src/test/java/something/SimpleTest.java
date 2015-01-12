037f70ffbb00ea3f695f6c402cb1c841b89f9396
sinan <sinangoo@gmx.de>
2015-01-12 09:47:13 +0100

    added another project

---
+++ simple_java_app_with_groovy_test/src/test/java/something/SimpleTest.java
@@ -0,0 +1,14 @@
+package something;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SimpleTest {
+
+    @Test
+    public void doSomeTest() {
+        System.out.println("SimpleTest:doSomeTest");
+        assertTrue(true);
+    }
+
+}
