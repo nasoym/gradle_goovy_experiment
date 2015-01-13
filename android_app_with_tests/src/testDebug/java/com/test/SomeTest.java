072109369b07f6dbd573c98529edf7320b5025ce
sinan <sinangoo@gmx.de>
2015-01-13 10:15:04 +0100

    added a real robolectric test

--- android_app_with_tests/src/testDebug/java/com/test/SomeTest.java
+++ android_app_with_tests/src/testDebug/java/com/test/SomeTest.java
@@ -1,11 +1,17 @@
 package com.test;
 
+import android.app.Activity;
+import android.util.Log;
+
+import java.util.*;
+
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
 
 @Config(emulateSdk = 18) 
 @RunWith(RobolectricTestRunner.class)
@@ -13,7 +19,15 @@ public class SomeTest {
 
     @Test
     public void test() {
-        assertTrue(true);
+        ActivityController<TestActivity> activityController = ActivityController.of(new TestActivity());
+        Activity activity = activityController
+            .attach()
+            .create()
+            .start()
+            .resume()
+            .visible()
+            .get();
+        assertNotNull(activity);
     }
 
 }
